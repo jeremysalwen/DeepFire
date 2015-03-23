199eb2ae55e4bbf9ac730d61820f059417c7f28e
Jeremy Salwen <jeremysalwen@gmail.com>
2015-03-23 01:23:51 -0400

    Add all includes to test file to ease testing

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,4 +1,8 @@
 #include "ActivationFunction.hpp"
 #include "ConnectionLayer.hpp"
+#include "Optimizers.hpp"
+#include "Dataset.hpp"
+#include "FeedForward.hpp"
+
 int main(int argc, char** argv) {
 }
